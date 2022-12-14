4a6e2615ed14a0ae70f5dac44fb861e127d34681
krishTechexpert <68417802+krishTechexpert@users.noreply.github.com>
2022-12-14 22:48:55 +0530

    very basic redux example with javascript

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-// Import stylesheets
-import './style.css';
+const initialValue={
+  count:0
+}
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+function rootReducer(state=initialValue,action){
+ 
+  if(action.type === 'increment'){
+    return {
+      ...state,
+      count:state.count + action.payload
+    }
+  }
+  if(action.type === 'decrement'){
+    return {
+      ...state,
+      count:state.count - action.payload
+    }
+  }
+  return state
+}
+
+const store = Redux.createStore(rootReducer)
+
+function showUI(){
+
+  document.getElementById('res').innerHTML=store.getState().count
+}
+
+showUI();
+
+store.subscribe(showUI)
+
+document.getElementById('inc').addEventListener('click',function(){
+  store.dispatch({type:'increment',payload:5})
+})
+
+document.getElementById('dec').addEventListener('click',function(){
+  if(store.getState().count <=0){
+    console.log('hi')
+    store.getState().count=0;
+  }else{
+    store.dispatch({type:'decrement',payload:5})
+
+  }
+
+})
